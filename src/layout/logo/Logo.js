83245e9bad8b2ddc17e3f2f0d0fea419ce800052
Rochmad <rochmad.wa@gmail.com>
2022-08-03 08:54:04 +0700

    feat: add logo

--- src/layout/logo/Logo.js
+++ src/layout/logo/Logo.js
@@ -9,7 +9,7 @@ const Logo = () => {
     <Link to={`${process.env.PUBLIC_URL}/`} className="logo-link">
       <img className="logo-light logo-img" src={LogoLight2x} alt="logo" />
       <img className="logo-dark logo-img" src={LogoDark2x} alt="logo" />
-      <img className="logo-small logo-img logo-img-small" src={LogoSmall} alt="logo" />
+      {/*<img className="logo-small logo-img logo-img-small" src={LogoSmall} alt="logo" />*/}
     </Link>
   );
 };
