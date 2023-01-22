9622e9c6e749fe07ae1c6cf389f84add004c58b5
Rochmad <rochmad.aji@munalively.com>
2023-01-22 07:55:55 +0700

    fix: add sorting in shop

--- src/functions/shop.js
+++ src/functions/shop.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getShops = async () => {
-  return await axios.get(`${process.env.REACT_APP_TUMBASPEDIA_API_URL}/shops?is_verified=false`, {
+  return await axios.get(`${process.env.REACT_APP_TUMBASPEDIA_API_URL}/shops?is_verified=false&sort_by=date&sort_type=desc`, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("accessToken")}`
     }
